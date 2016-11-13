49f55feb919650d52b848887357fefddf7cb57e8
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2016-11-13 12:45:23 -0500

    adding old assignments

---
+++ src/Leaderboard.js
@@ -0,0 +1,41 @@
+var gameInfo = function() {
+  return [
+   {
+     home_team: "Patriots",
+     away_team: "Broncos",
+     home_score: 7,
+     away_score: 3
+   },
+   {
+     home_team: "Broncos",
+     away_team: "Colts",
+     home_score: 3,
+     away_score: 0
+   },
+   {
+     home_team: "Patriots",
+     away_team: "Colts",
+     home_score: 11,
+     away_score: 7
+   },
+   {
+     home_team: "Steelers",
+     away_team: "Patriots",
+     home_score: 7,
+     away_score: 21
+   }
+ ]
+}
+
+// YOUR CODE HERE
+console.log("Teams\n-----");
+function Team(name, rank, wins, losses) = {
+  this.name = name;
+  this.rank = rank;
+  this.wins = wins;
+  this.losses = losses;
+}
+
+var = new Team ("The Mr.PoopyButtholes", "No 1 mofucka", "all", "NONE"};
+
+console.log(Team)
---
+++ src/Team.js
@@ -0,0 +1,11 @@
+// YOUR CODE HERE
+function Team(name) = {
+  this.name = name;
+  this.rank = Rank(name, info);
+  this.wins = wins;
+  this.losses = losses;
+}
+
+var = new Team ("The Mr.PoopyButtholes", "No 1 mofucka", "all", "NONE"}
+
+console.log(Team)
